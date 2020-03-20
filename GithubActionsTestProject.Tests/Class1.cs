d3df2a35e958951a952c7d7a1930c24835d81b64
Петряшов Андрей Николаевич <petryashov@kontur>
2020-03-20 20:40:25 +0500

    init

---
+++ GithubActionsTestProject.Tests/Class1.cs
@@ -0,0 +1,15 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+
+namespace GithubActionsTestProject.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Calculator.Plus(1, 3).Should().Be(4);
+        }
+    }
+}
---
+++ GithubActionsTestProject/Calculator.cs
@@ -0,0 +1,7 @@
+﻿namespace GithubActionsTestProject
+{
+    public class Calculator
+    {
+        public static int Plus(int a, int b) => a + b;
+    }
+}
